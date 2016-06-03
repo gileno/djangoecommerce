76a1ce75b070d0532fc47c8263634b3a2b91ea6f
Gileno Filho <gascf.cin@gmail.com>
2016-06-03 16:27:04 -0300

    implementando herança de templates

--- core/views.py
+++ core/views.py
@@ -5,9 +5,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    texts = ['Lorem ipsum', 'dolor sit amet', 'consectetur']
-    context = {
-        'title': 'django e-commerce',
-        'texts': texts
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -20,5 +20,8 @@ from core import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^contato/$', views.contact),
+    url(r'^produto/$', views.product),
+    url(r'^produtos/$', views.product_list),
     url(r'^admin/', admin.site.urls),
 ]
