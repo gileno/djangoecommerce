472ec75e3a942b59cfdd91ca398d6018511d1854
Gileno Filho <gascf.cin@gmail.com>
2016-06-03 16:57:04 -0300

    nomeando as url's

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from core import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^contato/$', views.contact),
-    url(r'^produto/$', views.product),
-    url(r'^produtos/$', views.product_list),
+    url(r'^$', views.index, name='index'),
+    url(r'^contato/$', views.contact, name='contact'),
+    url(r'^produto/$', views.product, name='product'),
+    url(r'^produtos/$', views.product_list, name='product_list'),
     url(r'^admin/', admin.site.urls),
 ]
