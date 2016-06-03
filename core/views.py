0259b95d7a51aa6b9b7a99d76ac915da381909aa
Gileno Filho <gascf.cin@gmail.com>
2016-06-03 15:33:20 -0300

    adicionando app core

---
+++ core/views.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World')
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from core.views import index
+
 urlpatterns = [
+    url(r'^$', index),
     url(r'^admin/', admin.site.urls),
 ]
