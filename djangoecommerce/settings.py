32fb33b82df4604c938928a26a4579db4f0e58c5
Gileno Filho <gascf.cin@gmail.com>
2016-06-05 17:42:33 -0300

    removendo comentários

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -124,18 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-# Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
