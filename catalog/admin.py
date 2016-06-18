03931f2ecc6c4bb68350a04f5669c28b05827028
Gileno Filho <gascf.cin@gmail.com>
2016-06-18 12:18:09 -0300

    customizando admin da app catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,5 +5,19 @@ from django.contrib import admin
 from .models import Product, Category
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'created', 'modified']
+    search_fields = ['name', 'slug']
+    list_filter = ['created', 'modified']
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'category', 'created', 'modified']
+    search_fields = ['name', 'slug', 'category__name']
+    list_filter = ['created', 'modified']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- catalog/apps.py
+++ catalog/apps.py
@@ -1,5 +1,8 @@
+# coding=utf-8
+
 from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
+    verbose_name = 'Catálogo'
