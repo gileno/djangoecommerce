2806d8d3e5990f293d142450bee96b2d04b35c73
Gileno Filho <gascf.cin@gmail.com>
2016-06-18 11:11:10 -0300

    adicionando app catalog e modelagem

---
+++ catalog/models.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+from django.db import models
+
+
+class Category(models.Model):
+
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['name']
+
+
+class Product(models.Model):
+
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+    category = models.ForeignKey('catalog.Category', verbose_name='Categoria')
+    description = models.TextField('Descrição', blank=True)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+        ordering = ['name']
--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'catalog',
 ]
 
 MIDDLEWARE_CLASSES = [
