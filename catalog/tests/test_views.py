6d990ccbd604cbea4130945db1a3b1a9ae3c919c
Gileno Filho <gascf.cin@gmail.com>
2016-06-26 12:12:45 -0300

    adicionando testes com paginação

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -25,6 +25,12 @@ class ProductListTestCase(TestCase):
 
     def test_context(self):
         response = self.client.get(self.url)
-        self.assertTrue('product_list' in response.context)
-        product_list = response.context['product_list']
-        self.assertEquals(product_list.count(), 10)
+        self.assertTrue('products' in response.context)
+        product_list = response.context['products']
+        self.assertEquals(product_list.count(), 3)
+        paginator = response.context['paginator']
+        self.assertEquals(paginator.num_pages, 4)
+
+    def test_page_not_found(self):
+        response = self.client.get('{}?page=5'.format(self.url))
+        self.assertEquals(response.status_code, 404)
