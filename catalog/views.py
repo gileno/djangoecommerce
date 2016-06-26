d3a61d1dde0adb87fdd6d04c076e5f8747d42d3c
Gileno Filho <gascf.cin@gmail.com>
2016-06-26 11:33:25 -0300

    cbv para listagem de produtos

--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,37 @@
 # coding=utf-8
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from .models import Product, Category
 
 
-def product_list(request):
-    context = {
-        'product_list': Product.objects.all()
-    }
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(generic.ListView):
+
+    queryset = Product.objects.all()
+    # model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
 
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {
-        'current_category': category,
-        'product_list': Product.objects.filter(category=category),
-    }
-    return render(request, 'catalog/category.html', context)
+product_list = ProductListView.as_view()
+
+
+class CategoryListView(generic.ListView):
+
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
+
+
+category = CategoryListView.as_view()
 
 
 def product(request, slug):
