0a6f2dd53b953434b76ecbac16b0b56f93eaca15
Gileno Filho <gascf.cin@gmail.com>
2016-07-03 18:42:42 -0300

    adicionando registro do usuário

--- core/views.py
+++ core/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
-from django.views.generic import View, TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import View, TemplateView, CreateView
+from django.contrib.auth import get_user_model
 
 from .forms import ContactForm
+
+
+User = get_user_model()
 
 
 class IndexView(TemplateView):
@@ -28,3 +34,14 @@ def contact(request):
         'success': success
     }
     return render(request, 'contact.html', context)
+
+
+class RegisterView(CreateView):
+
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    model = User
+    success_url = reverse_lazy('index')
+
+
+register = RegisterView.as_view()
