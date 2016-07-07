12b82845d49cb512f216b34f73c8c5415c760570
Gileno Filho <contato@gilenofilho.com.br>
2016-07-07 17:31:36 -0300

    adicionando alteração de dados para o usuário

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^alterar-dados/$', views.update_user, name='update_user'),
     url(r'^registro/$', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -22,5 +22,17 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('index')
 
 
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name', 'email']
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        return self.request.user
+
+
 index = IndexView.as_view()
 register = RegisterView.as_view()
+update_user = UpdateUserView.as_view()
