ea1d770057963259d7460544c56da8a8cad540c4
Gileno Filho <contato@gilenofilho.com.br>
2016-08-08 08:39:01 -0300

    testando o checkout

--- checkout/tests/test_models.py
+++ checkout/tests/test_models.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 
 from django.test import TestCase
+from django.conf import settings
 
 from model_mommy import mommy
 
-from checkout.models import CartItem
+from checkout.models import CartItem, Order
 
 
 class CartItemTestCase(TestCase):
@@ -17,3 +18,18 @@ class CartItemTestCase(TestCase):
         cart_item.quantity = 0
         cart_item.save()
         self.assertEquals(CartItem.objects.count(), 2)
+
+
+class OrderTestCase(TestCase):
+
+    def setUp(self):
+        self.cart_item = mommy.make(CartItem)
+        self.user = mommy.make(settings.AUTH_USER_MODEL)
+
+    def test_create_order(self):
+        Order.objects.create_order(self.user, [self.cart_item])
+        self.assertEquals(Order.objects.count(), 1)
+        order = Order.objects.get()
+        self.assertEquals(order.user, self.user)
+        order_item = order.items.get()
+        self.assertEquals(order_item.product, self.cart_item.product)
--- checkout/tests/test_views.py
+++ checkout/tests/test_views.py
@@ -2,6 +2,7 @@
 
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from model_mommy import mommy
 
@@ -32,3 +33,26 @@ class CreateCartItemTestCase(TestCase):
         response = self.client.get(self.url)
         cart_item = CartItem.objects.get()
         self.assertEquals(cart_item.quantity, 2)
+
+
+class CheckoutViewTestCase(TestCase):
+
+    def setUp(self):
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+        self.cart_item = mommy.make(CartItem)
+        self.client = Client()
+        self.checkout_url = reverse('checkout:checkout')
+
+    def test_checkou_view(self):
+        response = self.client.get(self.checkout_url)
+        redirect_url = '{}?next={}'.format(
+            reverse(settings.LOGIN_URL), self.checkout_url
+        )
+        self.assertRedirects(response, redirect_url)
+        self.client.login(username=self.user.username, password='123')
+        self.cart_item.cart_key = self.client.session.session_key
+        self.cart_item.save()
+        response = self.client.get(self.checkout_url)
+        self.assertTemplateUsed(response, 'checkout/checkout.html')
