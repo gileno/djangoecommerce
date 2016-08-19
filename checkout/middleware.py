e3b4f329e72551bc271921ee503e32e4251b71e5
Gileno Filho <contato@gilenofilho.com.br>
2016-08-19 09:18:08 -0300

    corrigindo middleware

--- checkout/middleware.py
+++ checkout/middleware.py
@@ -7,7 +7,7 @@ def cart_item_middleware(get_response):
     def middleware(request):
         session_key = request.session.session_key
         response = get_response(request)
-        if session_key != request.session.session_key:
+        if session_key != request.session.session_key and request.session.session_key:
             CartItem.objects.filter(cart_key=session_key).update(
                 cart_key=request.session.session_key
             )
