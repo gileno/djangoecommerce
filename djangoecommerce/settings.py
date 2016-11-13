065fe6d3dfa631e9d780657d121531a70c0a1ae2
Gileno Filho <contato@gilenofilho.com.br>
2016-11-13 19:33:35 -0300

    adicionando configurações para o amazon s3

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -176,12 +176,36 @@ PAGSEGURO_SANDBOX = True
 PAYPAL_TEST = True
 PAYPAL_EMAIL = 'contato@gilenofilho.com.br'
 
+# AWS
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+AWS_S3_SECURE_URLS = True
+AWS_QUERYSTRING_AUTH = False
+AWS_PRELOAD_METADATA = True
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = 'djangoecommerce'
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'djangoecommerce.s3util.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'djangoecommerce.s3util.MediaStorage'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+AWS_HEADERS = {
+    'x-amz-acl': 'public-read',
+    'Cache-Control': 'public, max-age=31556926'
+}
+
 # Thumbnails
 THUMBNAIL_ALIASES = {
     '': {
         'product_image': {'size': (285, 160), 'crop': True},
     },
 }
+THUMBNAIL_DEFAULT_STORAGE = DEFAULT_FILE_STORAGE
 
 try:
     from .local_settings import *
